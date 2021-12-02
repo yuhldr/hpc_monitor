f63696ab62db8ae955d60f0861ea5d2f560ce161
yuh <yuhldr@qq.com>
2021-12-02 18:01:37 +0800

    home磁盘检测

---
+++ disk.py
@@ -0,0 +1,64 @@
+import os, json, time
+import utils
+import config
+
+disk_part = "/home"
+home_user_path = "/home/data/disk_home/home_%s.txt" % (
+    utils.getTime("%Y-%m-%d"))
+log_file_name = "disk_home.log"
+config_path = "data/disk_home.json"
+
+utils.make_dir("data")
+
+disk_s = str(os.popen('df -h | grep "%s"' % disk_part).readline())
+# disk_data_root = str(os.system('df -h | grep /home'))
+
+disk_data_home = disk_s.split()
+
+print(disk_s)
+print(home_user_path)
+
+rate_now = int(disk_data_home[4].replace("%", ""))
+# rate_now = 98
+rates = config.warning_disk_home_rate
+
+last_mail_date = -1
+section_mail_date = disk_part + "_mail"
+config_data = {section_mail_date: {}}
+
+if os.path.exists(config_path):
+    config_data = json.load(open(config_path, "r"))
+
+print(config_data)
+
+for n in range(len(rates)):
+    i = len(rates) - 1 - n
+    if (str(i) in config_data[section_mail_date]):
+        last_mail_date = config_data[section_mail_date][str(i)]
+
+    if (rate_now >= rates[i] and utils.get_time_span(last_mail_date) >
+            config.warning_disk_home_time[i]):
+        title = config.warning_disk_home_title[i]
+
+        content = config.warning_disk_home_msg[
+            i] + "\n\n当前磁盘%s占用 %d：\n%s\n以下是每个用户详细使用情况：\n\n" % (
+                disk_part, rate_now, disk_s)
+
+        with open(home_user_path, "r") as file:
+            s = file.read()
+            content += s
+
+        utils.lzu_send_mails(title, content, config.mail_from_usr,
+                             config.mail_from_pw,
+                             config.disk_home_warning_to_mails)
+
+        break
+
+with open(config_path, 'w') as file:
+    config_data[section_mail_date][str(i)] = time.time()
+    json.dump(config_data, file, ensure_ascii=False)
+
+s = "分区：%s，占用率：%d，上次提醒时间：%s，间隔：%s" % (
+    disk_part, rate_now, utils.time2str(last_mail_date),
+    utils.get_time_friend_span(last_mail_date))
+utils.save_log2(s, log_file_name)
