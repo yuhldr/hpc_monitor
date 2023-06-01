5c33feac6601260e2824c38c02cf65ee8511449d
yuh <yuhldr@qq.com>
2023-06-01 16:17:38 +0800

    feat: 自动部署linux监测工具

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='ylt',
-                 version='0.0.2',
+                 version='0.0.3',
                  author="yuhldr",
                  author_email="***REMOVED***",
                  long_description=long_description,
