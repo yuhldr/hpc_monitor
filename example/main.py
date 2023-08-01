aded8d13c2e7c1e60230c7082327bf456856ed28
yuh <yuhldr@qq.com>
2023-08-01 11:14:23 +0800

    ns在线状态

--- example/main.py
+++ example/main.py
@@ -1,7 +1,15 @@
+'''文件入口'''
 from ylt import temps
 from ylt import disk
 from ylt import nodes
+from ylt import ns_state
 
-temps.main()
-disk.main()
-nodes.main()
+
+to_mail_users_disk=["***REMOVED***", "***REMOVED***", "***REMOVED***"]
+to_mail_users_temp=["***REMOVED***", "***REMOVED***"]
+to_mail_users_nodes=["***REMOVED***"]
+
+temps.main(to_mail_users_temp)
+disk.main(to_mail_users_disk)
+nodes.main(to_mail_users_nodes)
+ns_state.main()
