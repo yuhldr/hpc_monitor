af3e61b2235a9dcd6ad4189de1135db0b288ce15
yuhldr <yuhldr@qq.com>
2023-11-09 11:05:08 +0800

    style: 规范问题

--- ylt/utils/my_log.py
+++ ylt/utils/my_log.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 import time
-from ylt import CACHE_DIR, this_path
+
+from ylt import CACHE_DIR, THIS_PATH
 from ylt.utils.my_file import check_dir
 
-
-file_max_size = 1  # Mb，日志文件多大保存一次
-log_dir = this_path + "/log/"
+FILE_MAX_SIZE = 1  # Mb，日志文件多大保存一次
+LOG_DIR = THIS_PATH + "/log/"
 
 
 def save_log2(content_, log_file_name, test=False):
@@ -20,9 +20,9 @@ def save_log2(content_, log_file_name, test=False):
 
 
 def save_log(content, log_file_name):
-    check_dir(log_dir)
+    check_dir(LOG_DIR)
 
-    log_file = log_dir + log_file_name
+    log_file = LOG_DIR + log_file_name
 
     with open(log_file, 'a', encoding="utf-8") as file:
         file.write(content)
@@ -38,15 +38,15 @@ def file_size(log_file):
 
     fsize = os.path.getsize(log_file) / float(1024 * 1024)
 
-    if fsize > file_max_size:
+    if fsize > FILE_MAX_SIZE:
         shutil.move(log_file, ip_cache_dir_ + getTime() + ".log")
     return fsize
 
 
-unit_sec = 1
-unit_min = 60
-unit_hour = unit_min * 60
-unit_day = 24 * unit_hour
+UNIT_SEC = 1
+UNIT_MIN = 60
+UNIT_HOUR = UNIT_MIN * 60
+UNIT_DAY = 24 * UNIT_HOUR
 
 
 def getTime(p="%Y_%m_%d-%H_%M_%S"):
@@ -57,20 +57,20 @@ def time2str(time_long, p="%Y_%m_%d-%H_%M_%S"):
     return time.strftime(p, time.localtime(time_long))
 
 
-def get_time_span(t1, t2=time.time(), unit=unit_hour):
+def get_time_span(t1, t2=time.time(), unit=UNIT_HOUR):
     return (t2 - t1) / unit
 
 
 def get_time_friend_span(t1, t2=time.time()):
-    span_sec = get_time_span(t1, t2, unit_sec)
+    span_sec = get_time_span(t1, t2, UNIT_SEC)
     print(span_sec)
-    if (span_sec < unit_min):
-        return "%.3f s" % span_sec
-    elif (span_sec < unit_hour):
-        return "%.3f min" % (span_sec / unit_min)
-    elif (span_sec < unit_day):
-        return "%.3f h" % (span_sec / unit_hour)
-    elif (span_sec < unit_day * 30):
-        return "%.3f d" % (span_sec / unit_day)
+    if span_sec < UNIT_MIN:
+        return f"{span_sec:.3f} s"
+    elif span_sec < UNIT_HOUR:
+        return f"{(span_sec / UNIT_MIN):.3f} min"
+    elif span_sec < UNIT_DAY:
+        return f"{(span_sec / UNIT_HOUR):.3f} h"
+    elif span_sec < UNIT_DAY * 30:
+        return f"{(span_sec / UNIT_DAY):.3f} d"
     else:
         return time2str(t2)
